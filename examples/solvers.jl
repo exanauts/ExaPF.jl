6bc172994047c43c00632bd6e5520281c0a6450d
Michel Schanen <mschanen@anl.gov>
2020-05-24 21:15:15 -0500

    removed CUDA array allocations in CPU code

--- examples/solvers.jl
+++ examples/solvers.jl
@@ -14,8 +14,8 @@ outfile = "./plots/case14.txt"
 # bicgstabblocks = [8, 16, 32, 64, 128]
 # outfile = "./plots/Network_13R-015.txt"
 # datafile = "GO-Data/datasets/Trial_3_Real-Time/Network_30R-025/scenario_1/case.raw"
-# gmresblocks =    [8, 16, 32, 64, 128, 256]
-# bicgstabblocks = [8, 16, 32, 64, 128, 256]
+# gmresblocks =    [16, 32, 64, 128, 256]
+# bicgstabblocks = [16, 32, 64, 128, 256]
 # outfile = "./plots/Network_30R-025.txt"
 linsol_iters = []
 first_iters = []
@@ -25,7 +25,7 @@ for blocks in gmresblocks
   @time sol, conv, res, first_iter, linsol_iter = pf(datafile, blocks, "gmres")
   push!(linsol_iters, linsol_iter)
   push!(first_iters, first_iter)
-  println("first bicgstab iteration: $first_iter, total bicgstab iterations: $linsol_iter")
+  println("first gmres iteration: $first_iter, total gmres iterations: $linsol_iter")
 end
 push!(py, linsol_iters)
 push!(py, first_iters)
