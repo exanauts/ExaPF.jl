fe835dc9f93f3e3ed340ecae6383c527c5f7b8c8
fpacaud <francoispacaud8@gmail.com>
2020-08-18 08:58:39 -0500

    fix typo in evaluation of reduced cost

--- test/test_rgm.jl
+++ test/test_rgm.jl
@@ -10,7 +10,7 @@ import ExaPF: ParseMAT, PowerSystem, IndexSet
 @testset "RGM Optimal Power flow 9 bus case" begin
     datafile = "test/case9.m"
     pf = PowerSystem.PowerNetwork(datafile, 1)
-    
+
     # retrieve initial state of network
     pbus = real.(pf.sbus)
     qbus = imag.(pf.sbus)
@@ -31,16 +31,16 @@ import ExaPF: ParseMAT, PowerSystem, IndexSet
     function cost_x(xk)
         return ExaPF.cost_function(pf, xk, u, p; V=eltype(xk))
     end
-    
+
     function cost_u(uk)
         return ExaPF.cost_function(pf, xk, uk, p; V=eltype(uk))
     end
-    
+
     dCdx_fd = FiniteDiff.finite_difference_gradient(cost_x,xk)
     dCdx_ad = ForwardDiff.gradient(cost_x,xk)
     dCdu_fd = FiniteDiff.finite_difference_gradient(cost_u,u)
     dCdu_ad = ForwardDiff.gradient(cost_u,u)
-    
+
     @test isapprox(dCdx,dCdx_fd)
     @test isapprox(dCdu,dCdu_fd)
     @test isapprox(dCdx,dCdx_ad)
@@ -62,14 +62,14 @@ import ExaPF: ParseMAT, PowerSystem, IndexSet
         dCdx, dCdu = ExaPF.cost_gradients(pf, xk, uk, p)
 
         # evaluate cost
-        c = ExaPF.cost_function(pf, xk, u, p)
-        println("Cost: ", c)
+        c = ExaPF.cost_function(pf, xk, uk, p)
 
         # lamba calculation
         lambda = -(dGdx\dCdx)
 
         # compute gradient
         grad = dCdu + (dGdu')*lambda
+        println("Cost: ", c)
         println("Norm: ", norm(grad))
         # compute control step
         uk = uk - step*grad
