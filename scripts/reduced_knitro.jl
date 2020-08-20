ee77f42d1de3e2d15e58b0a77e1bad5a833ca9bb
fpacaud <francoispacaud8@gmail.com>
2020-08-20 20:13:56 +0200

    add script to solve reduced problem with Knitro

--- scripts/reduced_knitro.jl
+++ scripts/reduced_knitro.jl
@@ -4,7 +4,7 @@ using ExaPF
 using FiniteDiff
 using ForwardDiff
 using LinearAlgebra
-using Ipopt
+using KNITRO
 
 import ExaPF: ParseMAT, PowerSystem, IndexSet
 
@@ -24,7 +24,7 @@ function build_callback(pf, x0, u, p)
     function _update(u)
         # It looks like the tolerance of the Newton algorithm
         # could impact the convergence.
-        x, g, Jx, Ju, conv = ExaPF.solve(pf, xk, u, p, maxiter=200, tol=1e-14)
+        x, g, Jx, Ju, conv = ExaPF.solve(pf, xk, u, p, maxiter=50, tol=1e-14)
         dGdx = Jx(pf, x, u, p)
         dGdu = Ju(pf, x, u, p)
         # I like to live dangerously
@@ -40,15 +40,15 @@ function build_callback(pf, x0, u, p)
         (hash_u != hash(u)) && _update(u)
         c_u =  ExaPF.cost_function(pf, xk, u, p; V=eltype(u))
         # TODO: determine if we should include g(x, u), even if ≈ 0
-        return c_u + λk' * g(pf, xk, u, p)
+        return c_u #+ λk' * g(pf, xk, u, p)
     end
     function eval_grad_f(u, grad_f)
         (hash_u != hash(u)) && _update(u)
         # Update gradient
         cost_x = x_ -> ExaPF.cost_function(pf, x_, u, p; V=eltype(x_))
         cost_u = u_ -> ExaPF.cost_function(pf, xk, u_, p; V=eltype(u_))
-        fdCdx = xk -> ForwardDiff.gradient(cost_x, xk)
-        fdCdu = uk -> ForwardDiff.gradient(cost_u, uk)
+        fdCdx = x_ -> ForwardDiff.gradient(cost_x, x_)
+        fdCdu = u_ -> ForwardDiff.gradient(cost_u, u_)
         ∇fₓ = fdCdx(xk)
         ∇fᵤ = fdCdu(u)
 
@@ -56,8 +56,7 @@ function build_callback(pf, x0, u, p)
         # grad_f .= ∇fᵤ + S' * ∇fₓ
         # dCdx, dCdu = ExaPF.cost_gradients(pf, previous_x, u, p)
         # lamba calculation
-        λk = -(∇gₓ'\∇fₓ)
-        # compute reduced gradient
+        λk = -(∇gₓ')\∇fₓ
         grad_f .= ∇fᵤ + (∇gᵤ')*λk
         return nothing
     end
@@ -137,7 +136,7 @@ function build_callback(pf, x0, u, p)
     return eval_f, eval_grad_f, eval_g, eval_jac_g, eval_h
 end
 
-function run_reduced_ipopt(; hessian=false, cons=false)
+function run_reduced_knitro(; hessian=false, cons=false)
     datafile = "test/case9.m"
     # datafile = "test/case14.raw"
     # datafile = "../pglib-opf/pglib_opf_case1354_pegase.m"
@@ -173,7 +172,7 @@ function run_reduced_ipopt(; hessian=false, cons=false)
     x_U[1:nref] .= v_max
     # ... wrt. active power in PV buses
     x_L[nref + 1:nref + npv] .= 0.0
-    x_U[nref + 1:nref + npv] .= 2.5
+    x_U[nref + 1:nref + npv] .= 2.3
     # ... wrt. voltages in PV buses
     x_L[nref + npv + 1:nref + 2*npv] .= v_min
     x_U[nref + npv + 1:nref + 2*npv] .= v_max
@@ -199,37 +198,36 @@ function run_reduced_ipopt(; hessian=false, cons=false)
         hnnz = 0
         eval_h = nothing
     end
-
-    prob = Ipopt.createProblem(n, x_L, x_U, m, g_L, g_U, jnnz, hnnz,
-                         eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h)
-
-    prob.x = uk
-
-    # This tests callbacks.
-    function intermediate(alg_mod::Int, iter_count::Int,
-                          obj_value::Float64, inf_pr::Float64, inf_du::Float64, mu::Float64,
-                          d_norm::Float64, regularization_size::Float64, alpha_du::Float64, alpha_pr::Float64,
-                          ls_trials::Int)
-        return iter_count < 100  # Interrupts after one iteration.
+    function kn_eval_f(kc, cb, evalRequest, evalResult, userParams)
+        x = evalRequest.x
+        evalResult.obj[1] = eval_f(x)
+        return 0
     end
-
-    # I am too lazy to compute second order information
-    if !hessian
-        addOption(prob, "hessian_approximation", "limited-memory")
-        addOption(prob, "limited_memory_initialization", "scalar1")
-        # addOption(prob, "limited_memory_update_type", "sr1")
+    function kn_eval_g(kc, cb, evalRequest, evalResult, userParams)
+        x = evalRequest.x
+        eval_grad_f(x, evalResult.objGrad)
+        return 0
     end
-    # I am an agressive guy (and at some point computing more trial steps
-    # deteriorate the convergence)
-    addOption(prob, "accept_after_max_steps", 10)
-    # The derivative check is failing... so deactivate it
-    addOption(prob, "derivative_test", "first-order")
-    Ipopt.setIntermediateCallback(prob, intermediate)
+    println(eval_f(uk))
 
-    # println(xk)
-    Ipopt.solveProblem(prob)
-    return prob
+    kc = KNITRO.KN_new()
+    iᵤ = KNITRO.KN_add_vars(kc, n)
+    KNITRO.KN_set_var_lobnds(kc, iᵤ, x_L)
+    KNITRO.KN_set_var_upbnds(kc, iᵤ, x_U)
+    KNITRO.KN_set_var_primal_init_values(kc, iᵤ, uk)
+
+    cb = KNITRO.KN_add_objective_callback(kc, kn_eval_f)
+    KNITRO.KN_set_cb_grad(kc, cb, kn_eval_g)
+
+    KNITRO.KN_set_param(kc, "algorithm", 4)
+    KNITRO.KN_set_param(kc, "hessopt", 2)
+    KNITRO.KN_set_param(kc, "derivcheck", 1)
+    KNITRO.KN_set_param(kc, "derivcheck_type", 2)
+
+    KNITRO.KN_solve(kc)
+    res = KNITRO.KN_get_solution(kc)
+    KNITRO.KN_free(kc)
+    return res
 end
 
-prob = run_reduced_ipopt(hessian=false, cons=false)
-println(prob.x)
+prob = run_reduced_knitro(hessian=false, cons=false)
