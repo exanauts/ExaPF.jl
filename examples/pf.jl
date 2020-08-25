4db4d904013911c1987a40aff13011ed4effb9b2
Michel Schanen <mschanen@anl.gov>
2020-08-25 15:46:56 -0500

    pf example file updated

--- examples/pf.jl
+++ examples/pf.jl
@@ -9,13 +9,18 @@ import ExaPF: ParsePSSE, PowerSystem, IndexSet
 # datafile = "test/case14.raw"
 # npartition: Number of partitions for the additive Schwarz preconditioner
 function pf(datafile, npartition=2; solver="default", device = CPU())
-    # read data
-    println(solver)
+    # Create a powersystem object:
+    pf = ExaPF.PowerSystem.PowerNetwork(datafile, 1)
+
+    # Retrieve initial state of network
+    pbus = real.(pf.sbus)
+    qbus = imag.(pf.sbus)
+    vmag = abs.(pf.vbus)
+    vang = angle.(pf.vbus)
     
-    pf = PowerSystem.PowerNetwork(datafile)
-    x = ExaPF.PowerSystem.get_x(pf)
-    u = ExaPF.PowerSystem.get_u(pf)
-    p = ExaPF.PowerSystem.get_p(pf)
+    x = ExaPF.PowerSystem.get_x(pf, vmag, vang, pbus, qbus)
+    u = ExaPF.PowerSystem.get_u(pf, vmag, vang, pbus, qbus)
+    p = ExaPF.PowerSystem.get_p(pf, vmag, vang, pbus, qbus)
     return solve(pf, x, u, p; npartitions=npartition,
                                 solver=solver,
                                 device=device)
