89e55a44efc8bd6a1ff80139aecdee8edda8e446
fpacaud <francoispacaud8@gmail.com>
2020-09-03 07:50:54 -0500

    run reduced evaluator with Ipopt

--- scripts/reduced_ipopt.jl
+++ scripts/reduced_ipopt.jl
@@ -3,6 +3,7 @@ using Test
 using ExaPF
 using FiniteDiff
 using ForwardDiff
+using KernelAbstractions
 using LinearAlgebra
 using SparseArrays
 using Ipopt
@@ -31,137 +32,41 @@ function flow_limit(pf, x, u, p; T=Float64)
     return [cons_fr; cons_to]
 end
 
-function gₚ(pf, x, u, p; V=Float64)
-    nref = length(pf.ref)
-    npv = length(pf.pv)
-    ybus_re, ybus_im = ExaPF.Spmat{Vector}(pf.Ybus)
-
-    Vm, Va, pbus, qbus = PowerSystem.retrieve_physics(pf, x, u, p; V=V)
-    p_ref = zeros(V, nref)
-    q_pv = zeros(V, npv)
-
-    for (i, bus) in enumerate(pf.ref)
-        p_ref[i] = PowerSystem.get_power_injection(bus, Vm, Va, ybus_re, ybus_im)
-    end
-    for (i, bus) in enumerate(pf.pv)
-        q_pv[i] = PowerSystem.get_react_injection(bus, Vm, Va, ybus_re, ybus_im)
-    end
-    return [p_ref; q_pv]
-end
-
 # Build all the callbacks in a single closure.
-function build_callback(pf, x0, u, p)
-    nref = length(pf.ref)
-    npv = length(pf.pv)
-    npq = length(pf.pq)
-    nlines = size(pf.data["branch"], 1)
-    # Compute initial point.
-    xk, g, Jx, Ju, conv, resx! = ExaPF.solve(pf, x0, u, p, tol=1e-13)
-    ∇gₓ = Jx(pf, xk, u, p)
-    ∇gᵤ = Ju(pf, xk, u, p)
-    ∇fₓ, ∇fᵤ = ExaPF.cost_gradients(pf, xk, u, p)
-    λk = -(∇gₓ'\∇fₓ)
-    # Store initial hash.
+function build_callback(form, x0, u, p, constraints)
     hash_u = hash(u)
+    nlp = ExaPF.ReducedSpaceEvaluator(form, x0, u, p; constraints=constraints)
     function _update(u)
-        # It looks like the tolerance of the Newton algorithm
-        # could impact the convergence.
-        x, g, Jx, Ju, conv, resx! = ExaPF.solve(pf, xk, u, p, maxiter=50, tol=5e-12)
-        dGdx = Jx(pf, x, u, p)
-        dGdu = Ju(pf, x, u, p)
-        # I like to live dangerously
-        if !conv.has_converged
-            println(u)
-            println(xk)
-            println(conv.norm_residuals)
-            error("Fail to converge")
-        end
-        # Copy in closure's arrays
-        copy!(∇gₓ, dGdx)
-        copy!(∇gᵤ, dGdu)
-        copy!(xk, x)
         # Update hash
         hash_u = hash(u)
+        ExaPF.update!(nlp, u)
     end
     function eval_f(u)
         (hash_u != hash(u)) && _update(u)
-        c_u =  ExaPF.cost_function(pf, xk, u, p; V=eltype(u))
-        # TODO: determine if we should include g(x, u), even if ≈ 0
-        return c_u + λk' * g(pf, xk, u, p)
+        return ExaPF.objective(nlp, u)
     end
     function eval_grad_f(u, grad_f)
         (hash_u != hash(u)) && _update(u)
-        # Update gradient
-        cost_x = x_ -> ExaPF.cost_function(pf, x_, u, p; V=eltype(x_))
-        cost_u = u_ -> ExaPF.cost_function(pf, xk, u_, p; V=eltype(u_))
-        fdCdx = xk -> ForwardDiff.gradient(cost_x, xk)
-        fdCdu = uk -> ForwardDiff.gradient(cost_u, uk)
-        ∇fₓ = fdCdx(xk)
-        ∇fᵤ = fdCdu(u)
-
-        # lamba calculation
-        λk = -(∇gₓ'\∇fₓ)
-        # compute reduced gradient
-        grad_f .= ∇fᵤ + (∇gᵤ')*λk
+        ExaPF.gradient!(nlp, grad_f, u)
         return nothing
     end
     function eval_g(u, g)
         (hash_u != hash(u)) && _update(u)
-        # Constraints on vmag_{pq}
-        g[1:npq] .= xk[1:npq]
-        # Constraint on p_{ref}
-        g[npq+1:npq+nref+npv] .= gₚ(pf, xk, u, p)
-        g[npq+nref+npv+1:end] .= flow_limit(pf, xk, u, p)
+        ExaPF.constraint!(nlp, g, u)
         return nothing
     end
     function eval_jac_g(u::Vector{Float64}, mode, rows::Vector{Int32}, cols::Vector{Int32}, values::Vector{Float64})
         n = length(u)
-        m = npq + nref + npv + 2 * nlines
+        m = ExaPF.n_constraints(nlp)
         if mode == :Structure
-            idx = 1
-            for c in 1:m #number of constraints
-                for i in 1:n # number of variables
-                    rows[idx] = c ; cols[idx] = i
-                    idx += 1
-                end
-            end
+            r, c = ExaPF.jacobian_structure!(nlp)
+            rows .= r
+            cols .= c
         else
             (hash_u != hash(u)) && _update(u)
-            nx = length(xk)
-            n = length(u)
-            J = zeros(m, n)
-            rhs = zeros(nx)
-            λ = zeros(nx)
-            # Evaluate reduced Jacobian for bounds on vmag_{pq}
-            for ix in 1:npq
-                rhs .= 0.0
-                rhs[ix] = 1.0
-                λ .= - ∇gₓ' \ rhs
-                J[ix, :] .= ∇gᵤ' * λ
-            end
-            # Evaluate reduced Jacobian for bounds on p_{ref}
-            gg_x(x_) = gₚ(pf, x_, u, p; V=eltype(x_))
-            gg_u(u_) = gₚ(pf, xk, u_, p; V=eltype(u_))
-            jac_p_x = ForwardDiff.jacobian(gg_x, xk)
-            jac_p_u = ForwardDiff.jacobian(gg_u, u)
-            for ix in 1:(nref + npv)
-                rhs = jac_p_x[ix, :]
-                λ .= - ∇gₓ' \ rhs
-                J[npq + ix, :] .= jac_p_u[ix, :] + ∇gᵤ' * λ
-            end
-            # Evaluate reduced Jacobian for flow limits
-            hh_x(x_) = flow_limit(pf, x_, u, p; T=eltype(x_))
-            hh_u(u_) = flow_limit(pf, xk, u_, p; T=eltype(u_))
-            jac_h_x = ForwardDiff.jacobian(hh_x, xk)
-            jac_h_u = ForwardDiff.jacobian(hh_u, u)
-            _shift = npq + nref + npv
-            for ix in 1:2*nlines
-                rhs = jac_h_x[ix, :]
-                λ .= - ∇gₓ' \ rhs
-                J[_shift + ix, :] .= jac_h_u[ix, :] + ∇gᵤ' * λ
-            end
+            J = ExaPF.jacobian!(nlp, u)
 
-            @info("jac: ", J)
+            # @info("j", J)
             # Copy to Ipopt's Jacobian
             k = 1
             for i in 1:m
@@ -177,145 +82,62 @@ function build_callback(pf, x0, u, p)
                     rows::Vector{Int32}, cols::Vector{Int32}, obj_factor::Float64,
                     lambda::Vector{Float64}, values::Vector{Float64})
         n = length(u)
-        if mode == :Structure
-            index = 0
-            for i in 1:n
-                for j in i:n
-                    index += 1
-                    rows[index] = i
-                    cols[index] = j
-                end
-            end
-        else
-            (hash_u != hash(u)) && _update(u)
-            ## Hessian of cost function
-            cost_x = x_ -> ExaPF.cost_function(pf, x_, u, p; V=eltype(x_))
-            cost_u = u_ -> ExaPF.cost_function(pf, xk, u_, p; V=eltype(u_))
-            # Sensitivity matrix
-            S = - inv(Array(∇gₓ)) * ∇gᵤ
-            # ∂u²
-            H_uu = zeros(n, n)
-            ForwardDiff.hessian!(H_uu, cost_u, u)
-            # ∂x²
-            nx = length(xk)
-            H_xx = zeros(nx, nx)
-            ForwardDiff.hessian!(H_xx, cost_x, xk)
-            # ∂x∂u
-            function cross_f_xu(x, u)
-                cost_x = x_ -> ExaPF.cost_function(pf, x_, u, p; V=eltype(x_))
-                return ForwardDiff.gradient(cost_x, x)
-            end
-            #TODO
-            H_xu = FiniteDiff.finite_difference_jacobian(u_ -> cross_f_xu(xk, u_), u)
-
-            # Hessian of the equality constraint
-            # Evaluate constraints
-            vecx = [xk; u]
-            fjac = vecx -> ForwardDiff.jacobian(resx!, vecx)
-            jac = fjac(vecx)
-            jacx = sparse(jac[:,1:nx])
-            jacu = sparse(jac[:,nx+1:end])
-            hes = ForwardDiff.jacobian(fjac, vecx)
-            # I am not sure about the reshape.
-            # It could be that length(xk) goes at the end. This tensor stuff is a brain twister.
-            hes = reshape(hes, (nx, nx + n, nx + n))
-            ghesxx = hes[:, 1:nx, 1:nx]
-            ghesxu = hes[:, 1:nx, nx+1:end]
-            ghesuu = hes[:, nx+1:end, nx+1:end]
-            for i in 1:nx
-                H_uu .+= λk[i] * ghesuu[i, :, :]
-                H_xx .+= λk[i] * ghesxx[i, :, :]
-                H_xu .+= λk[i] * ghesxu[i, :, :]
-            end
-
-            # Global Hessian
-            H = H_uu + S'*H_xx*S + S'*H_xu + H_xu' * S
-            H = obj_factor * (H + H') / 2.0
-
-            index = 0
-            for i in 1:n
-                for j in i:n
-                    index += 1
-                    @inbounds values[index] = H[i, j]
-                end
-            end
-        end
+        # if mode == :Structure
+        #     index = 0
+        #     for i in 1:n
+        #         for j in i:n
+        #             index += 1
+        #             rows[index] = i
+        #             cols[index] = j
+        #         end
+        #     end
+        # else
+        #     (hash_u != hash(u)) && _update(u)
+        # end
         return nothing
     end
-    return eval_f, eval_grad_f, eval_g, eval_jac_g, eval_h
+    return nlp, eval_f, eval_grad_f, eval_g, eval_jac_g, eval_h
 end
 
 function run_reduced_ipopt(datafile; hessian=false, cons=false)
     pf = PowerSystem.PowerNetwork(datafile, 1)
-    nref = length(pf.ref)
-    npv = length(pf.pv)
-    npq = length(pf.pq)
-    nlines = size(pf.data["branch"], 1)
+    polar = PolarForm(pf, CPU())
+    x0 = ExaPF.initial(polar, State())
+    u0 = ExaPF.initial(polar, Control())
+    p = ExaPF.initial(polar, Parameters())
 
-    # retrieve initial state of network
-    pbus = real.(pf.sbus)
-    qbus = imag.(pf.sbus)
-    vmag = abs.(pf.vbus)
-    vang = angle.(pf.vbus)
-
-    x = ExaPF.PowerSystem.get_x(pf, vmag, vang, pbus, qbus)
-    u = ExaPF.PowerSystem.get_u(pf, vmag, vang, pbus, qbus)
-    p = ExaPF.PowerSystem.get_p(pf, vmag, vang, pbus, qbus)
-
-    xk = copy(x)
-    uk = copy(u)
+    xk = copy(x0)
+    uk = copy(u0)
     n = length(uk)
 
+    constraints = Function[ExaPF.state_constraint, ExaPF.power_constraints]
+    # constraints = Function[]
     # Build callbacks in closure
-    eval_f, eval_grad_f, eval_gg, eval_jac_gg, eval_hh = build_callback(pf, xk, uk, p)
-
-    u_min, u_max, x_min, x_max, p_min, p_max = ExaPF.get_bound_constraints(pf)
-    q_min, q_max = ExaPF.get_bound_reactive_power(pf)
+    nlp, eval_f, eval_grad_f, eval_g, eval_jac_g, eval_h = build_callback(polar, xk, uk, p, constraints)
+    m = ExaPF.n_constraints(nlp)
 
     # Set bounds on decision variable
-    x_L = u_min
-    x_U = u_max
+    x_L = nlp.u_min
+    x_U = nlp.u_max
+    g_L = nlp.g_min
+    g_U = nlp.g_max
 
-    F_max = get_lines_limit(pf)
-    # add constraint on PQ's voltage magnitude
-    if cons
-        m = npq + nref + npv + 2 * nlines
-        jnnz = m * n
-        zl = zeros(nlines)
-        g_L = [x_min[1:npq]; p_min; q_min; zl; zl]
-        g_U = [x_max[1:npq]; p_max; q_max; F_max; F_max]
-        eval_g = eval_gg
-        eval_jac_g = eval_jac_gg
-    else
-        m = 0
-        jnnz = 0
-        g_L = Float64[]
-        g_U = Float64[]
-        eval_g(u, g) = nothing
-        eval_jac_g(u::Vector{Float64}, mode, rows::Vector{Int32}, cols::Vector{Int32}, values::Vector{Float64}) = nothing
-    end
+    jnnz = n * m
+    hnnz = 0
 
     # Number of nonzeros in upper triangular Hessian
-    if hessian
-        hnnz = div(n * (n+1), 2)
-        eval_h = eval_hh
-    else
-        hnnz = 0
-        eval_h = nothing
-    end
-
     prob = Ipopt.createProblem(n, x_L, x_U, m, g_L, g_U, jnnz, hnnz,
                                eval_f, eval_g, eval_grad_f, eval_jac_g, eval_h)
 
-    # prob.x = (u_min .+ u_max) ./ 2.0
-    prob.x = uk
+    prob.x = (nlp.u_min .+ nlp.u_max) ./ 2.0
+    # prob.x = uk
 
     # This tests callbacks.
     function intermediate(alg_mod::Int, iter_count::Int,
                           obj_value::Float64, inf_pr::Float64, inf_du::Float64, mu::Float64,
                           d_norm::Float64, regularization_size::Float64, alpha_du::Float64, alpha_pr::Float64,
                           ls_trials::Int)
-        return iter_count < 200  # Interrupts after one iteration.
+        return iter_count < 500  # Interrupts after one iteration.
     end
 
     # I am too lazy to compute second order information
@@ -327,29 +149,29 @@ function run_reduced_ipopt(datafile; hessian=false, cons=false)
     end
     # addOption(prob, "accept_after_max_steps", 10)
     addOption(prob, "tol", 1e-2)
-    addOption(prob, "derivative_test", "first-order")
+    # addOption(prob, "derivative_test", "first-order")
     Ipopt.setIntermediateCallback(prob, intermediate)
 
     Ipopt.solveProblem(prob)
     u = prob.x
-    x, g, Jx, Ju, conv = ExaPF.solve(pf, xk, u, p, maxiter=50, tol=1e-14)
-    ExaPF.PowerSystem.print_state(pf, x, u, p)
-    res = flow_limit(pf, x, u, p) .- [F_max; F_max]
-    println(res)
-    @info("l", maximum(res))
-    println(findall(x -> x > 0, res))
+    # x, g, Jx, Ju, conv = ExaPF.solve(pf, xk, u, p, maxiter=50, tol=1e-14)
+    ExaPF.PowerSystem.print_state(pf, nlp.x, u, p)
+    # res = flow_limit(pf, x, u, p) .- [F_max; F_max]
+    # println(res)
+    # @info("l", maximum(res))
+    # println(findall(x -> x > 0, res))
     return prob, pf
 end
 
-datafile = "test/data/case9.m"
+# datafile = "test/data/case9.m"
 # datafile = "../pglib-opf/pglib_opf_case30_ieee.m"
 # datafile = "../pglib-opf/pglib_opf_case57_ieee.m"
-# datafile = "../pglib-opf/pglib_opf_case118_ieee.m"
+datafile = "../pglib-opf/pglib_opf_case118_ieee.m"
 # datafile = "../pglib-opf/pglib_opf_case300_ieee.m"
 # datafile = "../pglib-opf/pglib_opf_case1354_pegase.m"
 # datafile = "../pglib-opf/pglib_opf_case1888_rte.m"
 # datafile = "../pglib-opf/pglib_opf_case200_activ.m"
 # datafile = "../pglib-opf/pglib_opf_case500_goc.m"
-prob, pf = run_reduced_ipopt(datafile; hessian=true, cons=false)
+prob, pf = run_reduced_ipopt(datafile; hessian=false, cons=false)
 prob.status
 
