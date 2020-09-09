376e37de3a6f95064c842a3727d90dedca48dc9a
Adrian Maldonado <maldonadod@anl.gov>
2020-09-09 11:54:33 -0500

    updated test_rgm with new design

--- test/test_rgm.jl
+++ test/test_rgm.jl
@@ -4,6 +4,7 @@ using ExaPF
 using FiniteDiff
 using ForwardDiff
 using LinearAlgebra
+using KernelAbstractions
 
 # Include the linesearch here for now
 include("../src/algorithms/linesearches.jl")
@@ -13,46 +14,17 @@ import ExaPF: ParseMAT, PowerSystem, IndexSet
 @testset "RGM Optimal Power flow 9 bus case" begin
     datafile = joinpath(dirname(@__FILE__), "data", "case9.m")
     pf = PowerSystem.PowerNetwork(datafile, 1)
-
-    # retrieve initial state of network
-    pbus = real.(pf.sbus)
-    qbus = imag.(pf.sbus)
-    vmag = abs.(pf.vbus)
-    vang = angle.(pf.vbus)
-
-    x = ExaPF.PowerSystem.get_x(pf, vmag, vang, pbus, qbus)
-    u = ExaPF.PowerSystem.get_u(pf, vmag, vang, pbus, qbus)
-    p = ExaPF.PowerSystem.get_p(pf, vmag, vang, pbus, qbus)
-    u_min, u_max, x_min, x_max = ExaPF.PowerSystem.get_bound_constraints(pf)
+    polar = PolarForm(pf, CPU())
+    
+    xk = ExaPF.initial(polar, State())
+    uk = ExaPF.initial(polar, Control())
+    p = ExaPF.initial(polar, Parameters())
+    
+    constraints = Function[ExaPF.state_constraint, ExaPF.power_constraints]
+    nlp = @time ExaPF.ReducedSpaceEvaluator(polar, xk, uk, p; constraints=constraints)
 
     # solve power flow
-    xk, g, Jx, Ju, convergence = ExaPF.solve(pf, x, u, p)
-    dGdx = Jx(pf, x, u, p)
-    dGdu = Ju(pf, x, u, p)
-
-    c = ExaPF.cost_function(pf, xk, u, p)
-    dCdx, dCdu = ExaPF.cost_gradients(pf, xk, u, p)
-
-    # Test gradients
-    # We need uk here for the closure
-    uk = copy(u)
-    function cost_x(xk)
-        return ExaPF.cost_function(pf, xk, uk, p; V=eltype(xk))
-    end
-
-    function cost_u(uk)
-        return ExaPF.cost_function(pf, xk, uk, p; V=eltype(uk))
-    end
-
-    dCdx_fd = FiniteDiff.finite_difference_gradient(cost_x,xk)
-    dCdx_ad = ForwardDiff.gradient(cost_x,xk)
-    dCdu_fd = FiniteDiff.finite_difference_gradient(cost_u,u)
-    dCdu_ad = ForwardDiff.gradient(cost_u,u)
-
-    @test isapprox(dCdx,dCdx_fd)
-    @test isapprox(dCdu,dCdu_fd)
-    @test isapprox(dCdx,dCdx_ad)
-    @test isapprox(dCdu,dCdu_ad)
+    ExaPF.update!(nlp, uk)
 
     # reduced gradient method
     iterations = 0
@@ -62,41 +34,37 @@ import ExaPF: ParseMAT, PowerSystem, IndexSet
     norm_tol = 1e-5
 
     iter = 1
+
+    # initial gradient
+    grad = similar(uk)
+    fill!(grad, 0)
+
     while norm_grad > norm_tol && iter < iter_max
         println("Iteration: ", iter)
 
         # solve power flow and compute gradients
-        xk, g, Jx, Ju, convergence = ExaPF.solve(pf, xk, uk, p)
-        dGdx = Jx(pf, xk, uk, p)
-        dGdu = Ju(pf, xk, uk, p)
-        fdCdx = xk -> ForwardDiff.gradient(cost_x,xk)
-        fdCdu = uk -> ForwardDiff.gradient(cost_u,uk)
-        dCdx = fdCdx(xk)
-        dCdu = fdCdu(uk)
+        ExaPF.update!(nlp, uk)
 
         # evaluate cost
-        c = ExaPF.cost_function(pf, xk, uk, p)
+        c = ExaPF.objective(nlp, uk)
 
-        # lamba calculation
-        lambda = -(dGdx'\dCdx)
-
-        # Form functions
-        Lu = u -> ExaPF.cost_function(pf, xk, u, p) + (g(pf, xk, u, p))'*lambda
-        grad_Lu = u -> fdCdu(u) + (Ju(pf, xk, u, p)')*lambda
         # compute gradient
-        grad = grad_Lu(uk)
+        ExaPF.gradient!(nlp, grad, uk)
+        
         println("Cost: ", c)
         println("Norm: ", norm(grad))
+        
         # Optional linesearch
         # step = ls(uk, grad, Lu, grad_Lu)
         # compute control step
         uk = uk - step*grad
-        ExaPF.project_constraints!(uk, grad, u_min, u_max)
+        
+        ExaPF.project_constraints!(uk, grad, nlp.u_min, nlp.u_max)
         println("Gradient norm: ", norm(grad))
         norm_grad = norm(grad)
 
         iter += 1
     end
-    ExaPF.PowerSystem.print_state(pf, xk, uk, p)
+    ExaPF.PowerSystem.print_state(pf, nlp.x, uk, p)
 
 end
