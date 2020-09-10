28cf50c0d04dadea86557cc8b4612ffcacd49c85
Adrian Maldonado <maldonadod@anl.gov>
2020-09-09 22:24:04 -0500

    updated RGM with line-search

--- test/test_ffwu.jl
+++ test/test_ffwu.jl
@@ -5,11 +5,17 @@ using FiniteDiff
 using ForwardDiff
 using LinearAlgebra
 using Printf
+using KernelAbstractions
 #using UnicodePlots
 
 # Include the linesearch here for now
 import ExaPF: ParseMAT, PowerSystem, IndexSet
 
+# This function computes the Davidon cubic interpolation as seen in:
+# http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.693.272&rep=rep1&type=pdf
+# Notes:
+#
+#  - In some cases v^2 - F0p*FMp < 0. What to do if we need to keep alpha_m??
 function davidon_ls(pf, xk, uk, p, delta_x, delta_u, alpha_m)
 
     F0 = ExaPF.cost_function(pf, xk, uk, p)
@@ -25,7 +31,6 @@ function davidon_ls(pf, xk, uk, p, delta_x, delta_u, alpha_m)
     FMp = FiniteDiff.finite_difference_derivative(cost_a, alpha_m)
 
     v = (3.0/alpha_m)*(F0 - FM) + F0p + FMp
-
     w = sqrt(v^2 - F0p*FMp)
 
     scale = (FMp + w - v)/(FMp - F0p + 2*w)
@@ -34,12 +39,16 @@ function davidon_ls(pf, xk, uk, p, delta_x, delta_u, alpha_m)
     return alpha
 end
 
+# This function computes an approximation of delta_x
+# given delta_u
 function deltax_approx(delta_u, dGdx, dGdu)
     b = -dGdu*delta_u
     delta_x = dGdx\b
     return delta_x
 end
 
+# This function determines the descent direction given gradient
+# and limits on control variables.
 function descent_direction(pf, rk, u, u_min, u_max; damping=false)
 
     dim = length(u)
@@ -85,6 +94,7 @@ function descent_direction(pf, rk, u, u_min, u_max; damping=false)
     return delta_u
 end
 
+# This function determines if the algorithm has converged
 function check_convergence(rk, u, u_min, u_max; eps=1e-4)
     dim = length(rk)
 
@@ -130,6 +140,9 @@ function alpha_max(xk, delta_x, uk, delta_u, x_min, x_max, u_min, u_max)
             alpha_x = min(alpha_x, a_prop)
         end
     end
+    
+    # in some cases there seems to be no feasible step with the computed
+    # delta_x. maybe this is a problem with using linearized approximation.
     if alpha_x < 0.0
         return alpha_u
     end
@@ -137,7 +150,6 @@ function alpha_max(xk, delta_x, uk, delta_u, x_min, x_max, u_min, u_max)
 end
 
 # given limit alpha, compute costs along a direction.
-
 function cost_direction(pf, x, u, p, delta_u, alpha_max, alpha_dav; points=10)
 
     alphas = zeros(points)
@@ -153,9 +165,10 @@ function cost_direction(pf, x, u, p, delta_u, alpha_max, alpha_dav; points=10)
         costs[k] = c
         k += 1
     end
+    
+    # UNCOMMENT THESE LINES
     #plt = lineplot(alphas, costs, title = "Cost along alpha", width=80);
-    # plot a vertical line for Davidon's alpha
-    alpha_dav_vert = alpha_dav*ones(points)
+    #alpha_dav_vert = alpha_dav*ones(points)
     #scatterplot!(plt, alpha_dav_vert, costs)
     #println(plt)
 
@@ -164,59 +177,46 @@ end
 @testset "Two-stage OPF" begin
     datafile = joinpath(dirname(@__FILE__), "data", "case9.m")
     pf = PowerSystem.PowerNetwork(datafile, 1)
-
-    # retrieve initial state of network
-    pbus = real.(pf.sbus)
-    qbus = imag.(pf.sbus)
-    vmag = abs.(pf.vbus)
-    vang = angle.(pf.vbus)
-
-    x = ExaPF.PowerSystem.get_x(pf, vmag, vang, pbus, qbus)
-    u = ExaPF.PowerSystem.get_u(pf, vmag, vang, pbus, qbus)
-    p = ExaPF.PowerSystem.get_p(pf, vmag, vang, pbus, qbus)
-    u_min, u_max, x_min, x_max = ExaPF.PowerSystem.get_bound_constraints(pf)
-
-    # solve power flow
-    xk, g, Jx, Ju, convergence = ExaPF.solve(pf, x, u, p)
-    dGdx = Jx(pf, x, u, p)
-    dGdu = Ju(pf, x, u, p)
-
-    c = ExaPF.cost_function(pf, xk, u, p)
-    dCdx, dCdu = ExaPF.cost_gradients(pf, xk, u, p)
-
-    uk = copy(u)
+    polar = PolarForm(pf, CPU())
+    
+    xk = ExaPF.initial(polar, State())
+    uk = ExaPF.initial(polar, Control())
+    p = ExaPF.initial(polar, Parameters())
+    
+    constraints = Function[ExaPF.state_constraint, ExaPF.power_constraints]
+    nlp = @time ExaPF.ReducedSpaceEvaluator(polar, xk, uk, p; constraints=constraints)
+    jx, ju = ExaPF.init_ad_factory(polar, xk, uk, p)
 
     # reduced gradient method
     iterations = 0
-    iter_max = 100
+    iter_max = 4
     step = 0.0001
-    norm_grad = 10000
     converged = false
     norm_tol = 1e-5
 
+    # initialize arrays
+    grad = similar(uk)
+    fill!(grad, 0)
     cost_history = zeros(iter_max)
     grad_history = zeros(iter_max)
+
+    # retrieve bounds
+    u_min, u_max, x_min, x_max = ExaPF.PowerSystem.get_bound_constraints(pf)
 
     iter = 1
     while !converged && iter < iter_max
         println("Iteration: ", iter)
         # solve power flow and compute gradients
-        xk, g, Jx, Ju, convergence = ExaPF.solve(pf, xk, uk, p)
-        dGdx = Jx(pf, xk, uk, p)
-        dGdu = Ju(pf, xk, uk, p)
+        ExaPF.update!(nlp, uk)
 
         # evaluate cost
-        c = ExaPF.cost_function(pf, xk, uk, p; V=eltype(xk))
+        c = ExaPF.objective(nlp, uk)
         cost_history[iter] = c
-        dCdx, dCdu = ExaPF.cost_gradients(pf, xk, uk, p)
-
-        # lamba calculation
-        lambda = -(dGdx'\dCdx)
-
-        # Compute gradient
-        grad = dCdu + (dGdu')*lambda
         println("Cost: ", c)
-        println("Norm: ", norm(grad))
+
+        # compute gradient
+        ExaPF.gradient!(nlp, grad, uk)
+        println("Gradient norm: ", norm(grad))
 
         # check convergence
         converged = check_convergence(grad, uk, u_min, u_max)
@@ -224,25 +224,34 @@ end
         # compute descent direction
         delta_u = descent_direction(pf, grad, uk, u_min, u_max)
 
-        # line search
+        # compute gradients of G
+        dGdx = ExaPF.jacobian(polar, jx, nlp.x, uk, p)
+        dGdu = ExaPF.jacobian(polar, ju, nlp.x, uk, p)
+
+        # Line search
+        # 1st - compute approximate delta_x
         delta_x = deltax_approx(delta_u, dGdx, dGdu)
-        a_m = alpha_max(xk, delta_x, uk, delta_u, x_min, x_max, u_min, u_max)
+        # 2nd - compute a_m such that x + a_m*delta_x, u + a_m*delta_u
+        # does not violate constraints
+        a_m = alpha_max(nlp.x, delta_x, uk, delta_u, x_min, x_max, u_min, u_max)
+        # 3rd - fit cubic and find its minimum.
+        a_dav = davidon_ls(pf, nlp.x, uk, p, delta_x, delta_u, a_m)
+
         @printf("Maximal alpha: %f\n", a_m)
-        #a_dav = davidon_ls(pf, xk, uk, p, delta_x, delta_u, a_m)
-
-        #@printf("Davidon alpha: %f\n", a_dav)
-        #cost_direction(pf, xk, uk, p, delta_u, a_m, a_dav; points=10)
+        @printf("Davidon alpha: %f\n", a_dav)
+        
+        # Uncomment function below to plot the cost function along the descent direction
+        # and the calculated alpha.
+        #cost_direction(pf, nlp.x, uk, p, delta_u, a_m, a_dav; points=10)
+        
         # compute control step
-        println("Delta_u norm: ", norm(delta_u))
-        println(delta_u)
-        uk = uk + step*delta_u
-
-        println("Gradient norm: ", norm(grad))
-        norm_grad = norm(grad)
+        uk = uk + a_dav*delta_u
 
         iter += 1
     end
-    ExaPF.PowerSystem.print_state(pf, xk, uk, p)
+    ExaPF.PowerSystem.print_state(pf, nlp.x, uk, p)
+    
+    # uncomment to plot cost evolution
     #plt = lineplot(cost_history[1:iter - 1], title = "Cost history", width=80);
     #println(plt)
 
