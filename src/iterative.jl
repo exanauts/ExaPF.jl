8dcebe8a28a90618996960443ef96e8acba5e7d6
fpacaud <francoispacaud8@gmail.com>
2020-09-24 04:34:48 -0500

    bicgstab: optimize memory alloc in main loop

--- src/iterative.jl
+++ src/iterative.jl
@@ -9,6 +9,7 @@ using SparseArrays
 using TimerOutputs
 
 using ..ExaPF: Precondition
+import ..ExaPF: norm2
 
 export bicgstab, list_solvers
 
@@ -23,30 +24,24 @@ of nonsymmetric linear systems."
 SIAM Journal on scientific and Statistical Computing 13, no. 2 (1992): 631-644.
 """
 function bicgstab(A, b, P, xi, to::TimerOutput;
-                  tol=1e-6, maxiter=size(A, 1), verbose=false)
+                  tol=1e-10, maxiter=size(A, 1), verbose=false)
     # parameters
     n    = size(b, 1)
-    x0   = similar(b)
-    x0 = P * b
-    r0   = b - A * x0
-    br0  = copy(r0)
+    xi   = similar(b)
+    mul!(xi, P, b)
+    ri   = b - A * xi
+    br0  = copy(ri)
     rho0 = 1.0
     alpha = 1.0
     omega0 = 1.0
-    if A isa SparseArrays.SparseMatrixCSC
-        v0 = zeros(Float64, n)
-        p0 = zeros(Float64, n)
-    else
-        v0 = cuzeros(Float64, n)
-        p0 = cuzeros(Float64, n)
-    end
+    vi = similar(xi)
+    pi = similar(xi)
+    fill!(vi, 0)
+    fill!(pi, 0)
 
-    ri     = copy(r0)
     rhoi   = copy(rho0)
     omegai = copy(omega0)
-    vi = copy(v0)
-    pi = copy(p0)
-    xi .= x0
+    residual = copy(b)
 
     y = similar(pi)
     s = similar(pi)
@@ -55,7 +50,6 @@ function bicgstab(A, b, P, xi, to::TimerOutput;
     t2 = similar(pi)
     pi1 = similar(pi)
     vi1 = similar(pi)
-    xi1 = similar(pi)
     t = similar(pi)
 
     go = true
@@ -65,22 +59,26 @@ function bicgstab(A, b, P, xi, to::TimerOutput;
             @timeit to "First stage" begin
                 rhoi1 = dot(br0, ri) ; beta = (rhoi1/rhoi) * (alpha / omegai)
                 pi1 .= ri .+ beta .* (pi .- omegai .* vi)
-                y .= P * pi1
-                vi1 .= A * y
+                mul!(y, P, pi1)
+                mul!(vi1, A, y)
                 alpha = rhoi1 / dot(br0, vi1)
-                s .= ri .- (alpha * vi1)
-                z .= P * s
-                t .= A * z
-                t1 .= P * t
-                t2 .= P * s
+                s .= ri .- (alpha .* vi1)
+                mul!(z, P, s)
+                mul!(t, A, z)
+                mul!(t1, P, t)
+                mul!(t2, P, s)
             end
             @timeit to "Main stage" begin
                 omegai1 = dot(t1, t2) / dot(t1, t1)
-                xi1 .= xi .+ alpha .* y .+ omegai1 .* z
+                xi .= xi .+ alpha .* y .+ omegai1 .* z
             end
 
             @timeit to "End stage" begin
-                anorm = norm((A * xi1) .- b)
+                # TODO: should update to five arguments mul!
+                #       once CUDA.jl 1.4 is released
+                # mul!(residual, A, xi, 1.0, -1.0)
+                residual .= A * xi .- b
+                anorm = norm2(residual)
 
                 if verbose
                     println("\tIteration: ", iter)
@@ -103,7 +101,6 @@ function bicgstab(A, b, P, xi, to::TimerOutput;
                 pi     .= pi1
                 vi     .= vi1
                 omegai = omegai1
-                xi     .= xi1
                 iter   += 1
             end
         end
