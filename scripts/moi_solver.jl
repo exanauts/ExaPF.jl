6d750a33b2442a6aae8b4816178e0489749539d5
fpacaud <francoispacaud8@gmail.com>
2020-11-04 13:42:31 +0100

    nlp: add wrapper to MOI - part II

---
+++ scripts/moi_solver.jl
@@ -0,0 +1,42 @@
+
+using Ipopt
+using MathOptInterface
+
+const MOI = MathOptInterface
+
+function moi_solve(
+    optimizer::MOI.AbstractOptimizer,
+    nlp::ExaPF.AbstractNLPEvaluator,
+)
+    block_data = MOI.NLPBlockData(nlp)
+    u♭, u♯ = ExaPF.bounds(nlp)
+    u0 = ExaPF.initial(nlp)
+    n = ExaPF.n_variables(nlp)
+    vars = MOI.add_variables(optimizer, n)
+    # Set bounds and initial values
+    for i in 1:n
+        MOI.add_constraint(
+            optimizer,
+            MOI.SingleVariable(vars[i]),
+            MOI.LessThan(u♯[i])
+        )
+        MOI.add_constraint(
+            optimizer,
+            MOI.SingleVariable(vars[i]),
+            MOI.GreaterThan(u♭[i])
+        )
+        MOI.set(optimizer, MOI.VariablePrimalStart(), vars[i], u0[i])
+    end
+    MOI.set(optimizer, MOI.NLPBlock(), block_data)
+    MOI.set(optimizer, MOI.ObjectiveSense(), MOI.MIN_SENSE)
+    MOI.optimize!(optimizer)
+end
+
+datafile = joinpath(dirname(@__FILE__), "..", "test", "data", "case57.m")
+
+nlp = ExaPF.ReducedSpaceEvaluator(datafile)
+# optimizer = KNITRO.Optimizer(hessopt=3)
+optimizer = Ipopt.Optimizer(limited_memory_max_history=50, tol=1e-2)
+
+moi_solve(optimizer, nlp)
+
---
+++ src/Evaluators/MOI_wrapper.jl
@@ -0,0 +1,66 @@
+# MOI wrapper
+
+mutable struct ExaEvaluator <: MOI.AbstractNLPEvaluator
+    nlp::AbstractNLPEvaluator
+    hash_x::UInt
+end
+ExaEvaluator(nlp) = ExaEvaluator(nlp, UInt64(0))
+
+function _update!(ev::ExaEvaluator, x)
+    hx = hash(x)
+    if hx != ev.hash_x
+        update!(ev.nlp, x)
+        ev.hash_x = hx
+    end
+end
+
+MOI.features_available(::ExaEvaluator) = [:Grad]
+MOI.initialize(ev::ExaEvaluator, features) = nothing
+
+function MOI.jacobian_structure(ev::ExaEvaluator)
+    n = n_variables(ev.nlp)
+    m = n_constraints(ev.nlp)
+    jnnz = n * m
+    rows = zeros(Int, jnnz)
+    cols = zeros(Int, jnnz)
+    jacobian_structure!(ev.nlp, rows, cols)
+    return [(r, c) for (r, c) in zip(rows, cols)]
+end
+
+function MOI.eval_objective(ev::ExaEvaluator, x)
+    _update!(ev, x)
+    obj = objective(ev.nlp, x)
+    return obj
+end
+
+function MOI.eval_objective_gradient(ev::ExaEvaluator, g, x)
+    _update!(ev, x)
+    gradient!(ev.nlp, g, x)
+end
+
+function MOI.eval_constraint(ev::ExaEvaluator, cons, x)
+    _update!(ev, x)
+    constraint!(ev.nlp, cons, x)
+end
+
+function MOI.eval_constraint_jacobian(ev::ExaEvaluator, ∂cons, x)
+    _update!(ev, x)
+    n = n_variables(ev.nlp)
+    m = n_constraints(ev.nlp)
+    jac = zeros(m, n)
+    jacobian!(ev.nlp, jac, x)
+    k = 1
+    for i = 1:m
+        for j = 1:n
+            ∂cons[k] = jac[i, j]
+            k += 1
+        end
+    end
+end
+
+function MOI.NLPBlockData(nlp::AbstractNLPEvaluator)
+    lb, ub = nlp.g_min, nlp.g_max
+    ev = ExaEvaluator(nlp)
+    return MOI.NLPBlockData(MOI.NLPBoundsPair.(lb, ub), ev, true)
+end
+
