220140fb10a86774b31b1f05066227404af2f76a
fpacaud <francoispacaud8@gmail.com>
2021-07-23 15:44:23 +0200

    update documentation and remove deprecated scripts

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,8 @@
 using Pkg
 
-Pkg.develop(PackageSpec(path=joinpath(dirname(@__FILE__), "..")))
-# when first running instantiate
-Pkg.instantiate()
+# Pkg.develop(PackageSpec(path=joinpath(dirname(@__FILE__), "..")))
+# # when first running instantiate
+# Pkg.instantiate()
 
 using Documenter, ExaPF
 
@@ -25,14 +25,12 @@ makedocs(
             "Linear Solver" => "man/linearsolver.md",
             "PowerSystem" => "man/powersystem.md",
             "Formulations" => "man/formulations.md",
-            "Evaluators" => "man/evaluators.md",
         ],
         "Library" => [
             "AutoDiff" => "lib/autodiff.md",
             "Linear Solver" => "lib/linearsolver.md",
             "PowerSystem" => "lib/powersystem.md",
             "Formulations" => "lib/formulations.md",
-            "Evaluators" => "lib/evaluators.md",
         ]
     ]
 )
