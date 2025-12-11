49726dc2c1ccca001c92611aec79c3782a36564b
Michel Schanen <michel.schanen@gmail.com>
2025-12-11 08:09:27 -0600

    Fix example

--- examples/batch_power_flow.jl
+++ examples/batch_power_flow.jl
@@ -1,13 +1,34 @@
 using ExaPF
-using CUDA
-using CUDSS
+
 using LinearAlgebra
 const PS = ExaPF.PowerSystem
 
-res = run_pf("case9.m", CPU(), :block_polar, 10, ploads, qloads; verbose=2)
-sol_cpu = get_solution(res)
-isapprox(norm(sol_cpu), norm(sol_ref))
-res = run_pf("case9.m", CUDABackend(), :block_polar, 10, ploads, qloads; verbose=2)
-sol_gpu = get_solution(res)
+# Example of creating loads for multiple scenarios
+# ploads, and qloads are matrices of size (nbus, nscen)
+# This creates a equal loads across all scenarios for simplicity
+function create_loads(form, nscen::Int)
+    nbus = ExaPF.PowerSystem.get(form, ExaPF.PowerSystem.NumberOfBuses())
+    stack = ExaPF.NetworkStack(form)
+    pload = stack.params[1:nbus]
+    qload = stack.params[nbus+1:2*nbus]
+    ploads = zeros(Float64, nbus, nscen)
+    qloads = zeros(Float64, nbus, nscen)
+    for i in 1:nscen
+        ploads[:,i] .= pload
+    end
+    for i in 1:nscen
+        qloads[:,i] .= qload
+    end
+    return ploads, qloads
+end
 
-isapprox(norm(sol_gpu), norm(sol_ref))
+# 10 scenarios
+nscen = 10
+backend = CPU()  # Replace with CUDABackend() to run on NVIDIA GPU
+case = "case9.m"
+# Load case to use load data from the file
+polar = ExaPF.load_polar(case)
+ploads, qloads = create_loads(polar, nscen)
+println("Solve $nscen scenarios for case $case with on $backend)")
+res = run_pf(case, backend, :block_polar, nscen, ploads, qloads; verbose=2)
+sol_cpu = get_solution(res)
