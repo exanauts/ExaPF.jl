3faca823edbc023efe26194ca8f726b4bcd0d388
Michel Schanen <Michel.Schanen@gmail.com>
2025-12-23 08:27:17 -0600

    GH binary builder action for CPU

--- app/ExaPFApp/src/ExaPFApp.jl
+++ app/ExaPFApp/src/ExaPFApp.jl
@@ -2,12 +2,13 @@ module ExaPFApp
 
 using ExaPF
 using KernelAbstractions
-using CUDA, CUDSS
+# TODO: uncomment this when https://github.com/JuliaGPU/GPUCompiler.jl/issues/611 is fixed
+# using CUDA, CUDSS
 
-function run_pf_gpu(case::String)
-    println("Using GPU Backend (CUDA + CUDSS)")
-    run_pf(case, CUDABackend(), :polar; verbose=2)
-end
+# function run_pf_gpu(case::String)
+#     println("Using GPU Backend (CUDA + CUDSS)")
+#     run_pf(case, CUDABackend(), :polar; verbose=2)
+# end
 
 function run_pf_cpu(case::String)
     run_pf(case, CPU(), :polar; verbose=2)
@@ -17,7 +18,9 @@ function @main(args::Vector{String})::Int
     if args[2] == "cpu"
         run_pf_cpu(args[1])
     elseif args[2] == "gpu"
-        run_pf_gpu(args[1])
+        @error "GPU backend is not supported yet"
+        return 1
+        # run_pf_gpu(args[1])
     else
         @error "Unknown backend: $(args[2]). Use 'cpu' or 'gpu'."
         return 1
